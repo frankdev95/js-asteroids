7a7d397e2743c658de02aa697f279216fbbdd0f8
frankdev95 <frankdev95@protonmail.com>
2020-10-15 18:03:27 +0100

    Initialise project setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./sass/main.scss";
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const FPS = 60;
+
+const update = () => {
+    // DRAW SPACE
+
+    // DRAW SHIP
+
+    //
+}
+
+setInterval(update, 1000 / FPS);
