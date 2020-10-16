2b1636696ff9eac376ee3b16dd106ae4ec7729ab
frankdev95 <frankdev95@protonmail.com>
2020-10-16 11:34:54 +0100

    Add ship movement and hit edge functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
 import "./sass/main.scss";
+import {Ship} from "./ship";
+import {Global} from "./global";
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const FPS = 60;
 
+let keys = {}
+
+new Global(ctx, keys, FPS, canvas.width, canvas.height);
+
+let ship = new Ship(canvas.width / 2, canvas.height / 2, 30);
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft") keys[e.key] = true;
+    if(e.key === "ArrowRight") keys[e.key] = true;
+    if(e.key === " ") keys[e.key] = true;
+});
+document.addEventListener("keyup", (e) => {
+    if(e.key === "ArrowLeft") keys[e.key] = false;
+    if(e.key === "ArrowRight") keys[e.key] = false;
+    if(e.key === " ") keys[e.key] = false;
+});
+
+const renderSpace = () => {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 const update = () => {
-    // DRAW SPACE
+    //DRAW SPACE
+    renderSpace();
+    //DRAW SHIP
+    ship.draw();
+    ship.move();
+    //ROTATE SHIP
+    ship.rotate();
+    ship.isHitEdge();
+    //MOVE SHIP
 
-    // DRAW SHIP
-
-    //
 }
 
 setInterval(update, 1000 / FPS);
